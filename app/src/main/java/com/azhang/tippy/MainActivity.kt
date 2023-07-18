8adb9b926d41d6b32af3d99a67f9ddad00e606f8
Aaron Zhang <42594505+aarozhang@users.noreply.github.com>
2023-07-18 14:18:36 -0400

    Cleanup

--- app/src/main/java/com/azhang/tippy/MainActivity.kt
+++ app/src/main/java/com/azhang/tippy/MainActivity.kt
@@ -24,8 +24,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layout
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.Font
@@ -83,7 +81,6 @@ fun TippyApp() {
         Image(
             painter = painterResource(R.drawable.tippyblack),
             contentDescription = null,
-
             modifier = Modifier.padding(top = 32.dp)
         )
 
